90862c82d99a1a8bbab68b7f9792e8e98d536d2f
Balaji Seetharaman <balaji_ceg@outlook.com>
2024-01-28 19:46:53 +0530

    Update features.tsx

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -44,13 +44,7 @@ const features = [
 
 const Features = () => (
   <div className="features">
-    {features.map(({ text, icon: Icon }) => (
-      <div className="feature" key={text}>
-        {Icon && <Icon height={24} width={24} />}
-        <h4>{text}</h4>
-      </div>
-    ))}
   </div>
 )
 
-#export default Features
+export default Features
